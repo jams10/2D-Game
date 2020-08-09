25c4f23c77bd5b3cbd0833431461347b6e1cecad
jams10 <jamin3710@gmail.com>
2020-08-09 17:24:58 +0900

    Fixed Physics bug #2

--- Engine/Brick.cpp
+++ Engine/Brick.cpp
@@ -1,5 +1,6 @@
 #include "Brick.h"
 #include <assert.h>
+#include <cmath>
 
 Brick::Brick( const RectF& rect_in, Color color_in )
 	:
@@ -27,7 +28,13 @@ void Brick::ExecuteBallCollision( Ball& ball )
 	assert( CheckBallCollision( ball ) );
 
 	const Vec2 ballPos = ball.GetPosition();
-	if( ballPos.x >= rect.left && ballPos.x <= rect.right )
+	
+	// signbit : 매개변수가 음의 값이면 true 리턴.(cmath include 필요.)
+	if( std::signbit( ball.GetVelocity().x ) == std::signbit( (ballPos - GetCenter()).x ) )
+	{
+		ball.ReboundY();
+	}
+	else if( ballPos.x >= rect.left && ballPos.x <= rect.right )
 	{
 		ball.ReboundY();
 	}
